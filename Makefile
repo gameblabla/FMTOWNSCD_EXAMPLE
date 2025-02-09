b4a5a90ff8665705f17b6013464a409aabcb8704
gameblabla <gameblabla@protonmail.com>
2025-02-09 04:18:15 +0100

    Update

--- Makefile
+++ Makefile
@@ -1,9 +1,15 @@
 CC = i386-pc-run386-gcc
+AS = i386-pc-run386-as
 CFLAGS = -O2 -Wall -Icommon
-
+ASFLAGS = -march=i386
 SRC_DIRS = common src
-SRCS = $(wildcard $(patsubst %,%/*.c,$(SRC_DIRS)))
+
+# Find all C and assembly source files
+SRCS = $(wildcard $(patsubst %,%/*.c,$(SRC_DIRS))) $(wildcard $(patsubst %,%/*.s,$(SRC_DIRS)))
+
+# Generate object file names
 OBJS = $(SRCS:.c=.o)
+OBJS := $(OBJS:.s=.o)
 
 TARGET = FROG
 
@@ -11,12 +17,17 @@ all: $(TARGET)
 
 $(TARGET): $(OBJS)
 	$(CC) $(CFLAGS) -o $@ $^
-	rm CD/$(TARGET).EXP
+	rm -f CD/$(TARGET).EXP
 	elf2exp $(TARGET) CD/$(TARGET).EXP
-	rm $(TARGET)
+	rm -f $(TARGET)
 
+# Compile C files
 %.o: %.c
 	$(CC) $(CFLAGS) -c -o $@ $<
+
+# Assemble assembly files
+%.o: %.s
+	$(AS) $(ASFLAGS) -o $@ $<
 
 clean:
 	rm -f $(OBJS) $(TARGET)
--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
+#include <stdio.h>
 
+#include "defs.h"
 #include "common.h"
 #include "settings.h"
 #include "palette.h"
@@ -43,21 +45,61 @@ static const uint16_t crtc_set_31[32] = {
     0x0192  // CR2  = 402
 };
 
-
+// 4bpp color mode
 static const uint8_t video_set_31[2] = { 0x0f, 0x08 };
 
-static crtc_set_t crtc = CRTC_SET_1;
-static video_set_t video = VIDEO_SET_1;
+static const uint16_t crtc_frog[32] = {
+    0x0074, // HSW1 = 116
+    0x0530, // HSW2 = 1328
+    0x0000, // RSV0 = 0
+    0x0000, // RSV1 = 0
+    0x0617, // HST  = 1559
+    0x0006, // VST1 = 6
+    0x000C, // VST2 = 12
+    0x0012, // EET  = 18
+    0x020B, // VST  = 523
+    0x00E7, // HDS0 = 231
+    0x05E7, // HDE0 = 1511
+    0x00E7, // HDS1 = 231
+    0x05E7, // HDE1 = 1511
+    0x002A, // VDS0 = 42
+    0x020A, // VDE0 = 522
+    0x002A, // VDS1 = 42
+    0x020A, // VDE1 = 522
+    0x0000, // FA0  = 0
+    0x00E7, // HAJ0 = 231
+    0x0000, // FO0  = 0
+    0x0100, // LO0  = 256
+    0x0000, // FA1  = 0
+    0x00E7, // HAJ1 = 231
+    0x0000, // FO1  = 0
+    0x0100, // LO1  = 256
+    0x0056, // EHAJ = 86
+    0x0001, // EVAJ = 1
+    0x0303, // ZOOM = 771
+    0x8005, // CR0  = 32773
+    0x0001, // CR1  = 1
+    0x0002, // FR   = 0
+    0x0188  // CR2  = 392
+};
+
+// 16-bits color
+static const uint8_t video_frog[2] = { 0x1f, 0x28 };
+
 
 int main(int argc, char *argv[]) {
-	volatile uint32_t *vram = (uint32_t *)0x80000000;
-	volatile uint32_t *vram2 = (uint32_t *)0x80040000;
-	   
+	volatile uint16_t *vram = (uint16_t *)0x80000000;
+	volatile uint16_t *vram2 = (uint16_t *)0x80040000;
+	
+	_outb(IO_FMR_GVRAMDISPMODE, 0x27);
+	_outb(IO_FMR_VRAM_OR_MAINRAM, 0);
+	_outb(IO_FMR_GVRAMMASK, 0x0F);	  
+	 
 	stop_display();
 	
 	for(int i=0;i<32;i++)
 	{
-		crtc_out16(i, crtc_set_31[i]);
+		crtc_out16(i, crtc_frog[i]);
 	}
 	
 	for(int i=0;i<2;i++)
@@ -74,10 +116,9 @@ int main(int argc, char *argv[]) {
 	
 
 	const uint32_t *src_line = picture;
-	for (int i = 0; i < (512*480)/4; i++) 
+	for (int i = 0; i < 320*240; i++) 
 	{
-		*vram++ = *src_line++;
-		*vram2++ = *src_line++;
+		vram[i] = 0x2222;
 	}
 	
 	while(1)
